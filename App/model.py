ba7c4e62f836bc13c5a64cfda80b6bf653cd23c2
OscarEduardoMartinezAcevedo <o.martineza@uniandes.edu.co>
2020-10-14 23:25:37 -0500

    .

--- App/model.py
+++ App/model.py
@@ -58,7 +58,7 @@ def newAnalyzer():
                 }
 
     analyzer['crimes'] = lt.newList('SINGLE_LINKED', compareIds)
-    analyzer['dateIndex'] = om.newMap(omaptype='RBT',
+    analyzer['dateIndex'] = om.newMap(omaptype='BST',
                                       comparefunction=compareDates)
     return analyzer
 
